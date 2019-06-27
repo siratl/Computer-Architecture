77e21464e9f2d095d8d694f5a9372c6a0d039f79
Elisha Atulomah <elishaubax@gmail.com>
2019-06-27 14:21:08 -0700

    Implement struct cpu in cpu.h

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -2,17 +2,20 @@
 #define _CPU_H_
 
 // Holds all information about the CPU
-struct cpu {
-  // TODO
-  // PC
-  // registers (array)
-  // ram (array)
+struct cpu
+{
+  unsigned int pc;
+  // registers (array) -- R0->R7 (8 items)
+  unsigned char reg[8];
+  // ram (array) -- With 8 bits, our CPU has a total of 256 bytes of memory
+  unsigned char ram[256];
 };
 
 // ALU operations
-enum alu_op {
-	ALU_MUL
-	// Add more here
+enum alu_op
+{
+  ALU_MUL
+  // Add more here
 };
 
 // Instructions
@@ -20,8 +23,8 @@ enum alu_op {
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
 
-#define LDI  0b10000010
-#define PRN  0b01000111
+#define LDI 0b10000010
+#define PRN 0b01000111
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
